fbc26b7ba2f23b75b2d27b3d671c66e4b549641c
Márcio <marciopro3@gmail.com>
2023-12-27 16:25:36 -0300

    Meu primeiro commit

---
+++ Backend/index.js
@@ -0,0 +1,111 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mysql from 'mysql'
+const app = express()
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '134679',
+    database: 'cadastro'
+
+});
+
+let usuarios = [
+
+    {
+        id: 1,
+        nome: 'Marcio'
+    },
+    {
+        id: 2,
+        nome: 'Rafael'
+    },
+
+]
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+
+    res.send('Teste01')
+
+})
+
+app.get('/db', (req, res) => {
+
+
+    connection.connect(function (err) {
+        if (err) {
+            res.send('erro de conexão: ' + err.stack)
+            return;
+        }
+        res.send('Conectado com o id: ' + connection.threadId);
+    })
+
+})
+
+app.get('/usuario', (req, res) => {
+
+    connection.query('SELECT * FROM usuario', function (error, results, fields) {
+        if (error) {
+            res.send('Erro')
+            return
+        }
+        res.send(results)
+    })
+})
+
+app.get('/usuario/:id', (req, res) => {
+    const id = req.params.id
+
+    connection.query(`SELECT * FROM usuario WHERE id = ${id}`, function (error, results, fields) {
+        if (error) {
+            res.send('Erro')
+            return
+        }
+        res.send(results)
+    })
+})
+
+app.post('/usuario', (req, res) => {
+    const nome = req.body.nome
+    connection.query(`INSERT INTO usuario (nome) VALUES ('${nome}')`, function (error, results, fields) {
+        if (error) {
+            res.send('Erro')
+            return
+        }
+        res.send(results)
+    })
+})
+
+app.put('/usuario/:id', (req, res) => {
+    const id = req.params.id
+    const nome = req.body.nome
+    connection.query(`UPDATE usuario SET nome = '${nome}' WHERE id = ${id}`, function (error, results, fields) {
+        if (error) {
+            res.send('Erro')
+            return
+        }
+        res.send(results)
+    })
+
+})
+
+app.delete('/usuario/:id', (req, res) => {
+    const id = req.params.id
+    connection.query(`DELETE FROM usuario WHERE id = ${id}`, function (error, results, fields) {
+        if (error) {
+            res.send('Erro')
+            return
+        }
+        res.send(results)
+    })
+
+})
+
+app.listen(3000, () => {
+
+    console.log('Servidor rodando na porta 3000')
+
+})
